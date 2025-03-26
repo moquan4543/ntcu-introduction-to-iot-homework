98758e405be8e3c828ba8d31f12dc8f678211ebb
moquan4543 <acs111104@gm.ntcu.edu.tw>
2025-03-27 01:14:56 +0800

    rename url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NTCU_IntroductionToIoT_Homework"
+  base: "/ntcu-introduction-to-iot-homework"
 })
